c1bb3e124b4955b23ef1043e3b02c75977a9081d
Writtentsabi <anastasistsaboukos@gmail.com>
2025-12-27 13:06:04 +0200

    Fix safety settings to BLOCK_NONE

--- server.js
+++ server.js
@@ -21,8 +21,24 @@ const ai = new GoogleGenAI( {
 
 const safety = [
   {
+    category: "HARM_CATEGORY_HARASSMENT",
+    threshold: "BLOCK_NONE",
+  },
+  {
+    category: "HARM_CATEGORY_HATE_SPEECH",
+    threshold: "BLOCK_NONE",
+  },
+  {
     category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    threshold: "OFF",
+    threshold: "BLOCK_NONE",
+  },
+  {
+    category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+    threshold: "BLOCK_NONE",
+  },
+  {
+    category: "HARM_CATEGORY_CIVIC_INTEGRITY",
+    threshold: "BLOCK_NONE",
   }
 ];
 
