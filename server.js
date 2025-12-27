aa7dc37e34748284fa416d7b8e79aece16aeb512
Writtentsabi <anastasistsaboukos@gmail.com>
2025-12-27 15:08:24 +0200

    Ενημέρωση server.js για διορθωση του sefety

--- server.js
+++ server.js
@@ -13,7 +13,25 @@ const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY
 });
 
-const safety =;
+const safety = [
+  {
+    category: "HARM_CATEGORY_HARASSMENT",
+    threshold: "BLOCK_NONE",
+  },
+  {
+    category: "HARM_CATEGORY_HATE_SPEECH",
+    threshold: "BLOCK_NONE",
+  },
+  {
+    category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    threshold: "BLOCK_NONE",
+  },
+  {
+    category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+    threshold: "BLOCK_NONE",
+  },
+];
+
 
 app.use(express.static('public'));
 app.use(express.json({ limit: '50mb' }));
