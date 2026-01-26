538cbc78a625a13708fee7ae588ed0dcf900aee1
Writtentsabi <anastasistsaboukos@gmail.com>
2026-01-26 18:03:55 +0200

    fix: image generation's output reverted back to only image

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.post('/api/chat', async (req, res) => {
 				contents: contents,
 				config: {
 					systemInstruction: IMAGE_SYSTEM_INSTRUCTION,
-					responseModalities: ['IMAGE','TEXT'],
+					responseModalities: ['IMAGE'],
 					safetySettings: safety,
 					imageConfig: {
 						aspectRatio: aspectRatio || "1:1"
